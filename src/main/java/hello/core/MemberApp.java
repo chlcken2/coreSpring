7fffda9aecf99e2bafc3f1cc02d1be70764429bf
chl.cken <ckdgus988@naver.com>
2022-03-25 15:54:10 +0900

    빈 조회(이름, 타입, 존재x) 경우 Test 케이스 작성

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,11 +4,17 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //AppConfig에 있는 클래스를 @어노테이션을 스프링 컨테이너에 넣고 관리해줌
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); //이름 / 타입
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
